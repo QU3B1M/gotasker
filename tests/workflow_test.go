0666008d21c3e36a68842b9711513c5839ccfa1a
quebim <kevinledesmam95@gmail.com>
2024-02-27 21:03:53 -0300

    Add unit tests for workflows ProcessWorkflow function

--- tests/workflow_test.go
+++ tests/workflow_test.go
@@ -187,3 +187,253 @@ func TestConvertKeysToStringWithSliceOfMap(t *testing.T) {
 		t.Errorf("Expected %v, but got %v", expected, actual)
 	}
 }
+
+func TestProcessWorkflow(t *testing.T) {
+	input := map[string]interface{}{
+		"variables": map[string]interface{}{
+			"var1": "value1",
+			"var2": "value2",
+		},
+		"tasks": []interface{}{
+			map[string]interface{}{
+				"task": "task1",
+				"key1": "{{ .var1 }}",
+				"key2": "{{ .var2 }}",
+			},
+		},
+	}
+	expected := []map[string]interface{}{
+		{
+			"task": "task1",
+			"key1": "value1",
+			"key2": "value2",
+		},
+	}
+	actual := workflow.ProcessWorkflow(input)
+	if !reflect.DeepEqual(expected, actual) {
+		t.Errorf("Expected %v, but got %v", expected, actual)
+	}
+}
+
+func TestProcessWorkflowWithForeach(t *testing.T) {
+	input := map[string]interface{}{
+		"variables": map[string]interface{}{
+			"var1": []interface{}{"var1-value1", "var1-value2"},
+			"var2": []interface{}{"var2-value1", "var2-value2"},
+		},
+		"tasks": []interface{}{
+			map[string]interface{}{
+				"task": "task1",
+				"key1": "{{ .v1 }}",
+				"key2": "{{ .v2 }}",
+				"foreach": []interface{}{
+					map[string]interface{}{
+						"variable": "var1",
+						"as":       "v1",
+					},
+					map[string]interface{}{
+						"variable": "var2",
+						"as":       "v2",
+					},
+				},
+			},
+		},
+	}
+	expected := []map[string]interface{}{
+		{
+			"task": "task1",
+			"key1": "var1-value1",
+			"key2": "var2-value1",
+		},
+		{
+			"task": "task1",
+			"key1": "var1-value1",
+			"key2": "var2-value2",
+		},
+		{
+			"task": "task1",
+			"key1": "var1-value2",
+			"key2": "var2-value1",
+		},
+		{
+			"task": "task1",
+			"key1": "var1-value2",
+			"key2": "var2-value2",
+		},
+	}
+	actual := workflow.ProcessWorkflow(input)
+	if !reflect.DeepEqual(expected, actual) {
+		t.Errorf("Expected %v, but got %v", expected, actual)
+	}
+}
+
+func TestProcessWorkflowWithNestedForeach(t *testing.T) {
+	input := map[string]interface{}{
+		"variables": map[string]interface{}{
+			"var1": []interface{}{"var1-value1", "var1-value2"},
+			"var2": []interface{}{"var2-value1", "var2-value2"},
+			"var3": []interface{}{"var3-value1", "var3-value2"},
+		},
+		"tasks": []interface{}{
+			map[string]interface{}{
+				"task": "task1",
+				"key1": "{{ .v1 }}",
+				"key2": "{{ .v2 }}",
+				"foreach": []interface{}{
+					map[string]interface{}{
+						"variable": "var1",
+						"as":       "v1",
+					},
+					map[string]interface{}{
+						"variable": "var2",
+						"as":       "v2",
+					},
+				},
+			},
+		},
+	}
+	expected := []map[string]interface{}{
+		{
+			"task": "task1",
+			"key1": "var1-value1",
+			"key2": "var2-value1",
+		},
+		{
+			"task": "task1",
+			"key1": "var1-value1",
+			"key2": "var2-value2",
+		},
+		{
+			"task": "task1",
+			"key1": "var1-value2",
+			"key2": "var2-value1",
+		},
+		{
+			"task": "task1",
+			"key1": "var1-value2",
+			"key2": "var2-value2",
+		},
+	}
+	actual := workflow.ProcessWorkflow(input)
+	if !reflect.DeepEqual(expected, actual) {
+		t.Errorf("Expected %v, but got %v", expected, actual)
+	}
+}
+
+func TestProcessWorkflowWithNestedForeachAndVariables(t *testing.T) {
+	input := map[string]interface{}{
+		"variables": map[string]interface{}{
+			"var1": []interface{}{"var1-value1", "var1-value2"},
+			"var2": []interface{}{"var2-value1", "var2-value2"},
+			"var3": []interface{}{"var3-value1", "var3-value2"},
+		},
+		"tasks": []interface{}{
+			map[string]interface{}{
+				"task": "task1",
+				"key1": "{{ .v1 }}",
+				"key2": "{{ .v2 }}",
+				"foreach": []interface{}{
+					map[string]interface{}{
+						"variable": "var1",
+						"as":       "v1",
+					},
+					map[string]interface{}{
+						"variable": "var2",
+						"as":       "v2",
+						"foreach": []interface{}{
+							map[string]interface{}{
+								"variable": "var3",
+								"as":       "v3",
+							},
+						},
+					},
+				},
+			},
+		},
+	}
+	expected := []map[string]interface{}{
+		{
+			"task": "task1",
+			"key1": "var1-value1",
+			"key2": "var2-value1",
+		},
+		{
+			"task": "task1",
+			"key1": "var1-value1",
+			"key2": "var2-value2",
+		},
+		{
+			"task": "task1",
+			"key1": "var1-value2",
+			"key2": "var2-value1",
+		},
+		{
+			"task": "task1",
+			"key1": "var1-value2",
+			"key2": "var2-value2",
+		},
+	}
+	actual := workflow.ProcessWorkflow(input)
+	if !reflect.DeepEqual(expected, actual) {
+		t.Errorf("Expected %v, but got %v", expected, actual)
+	}
+}
+
+func TestProcessWorkflowWithNestedForeachAndVariablesAndMap(t *testing.T) {
+	input := map[string]interface{}{
+		"variables": map[string]interface{}{
+			"var1": []interface{}{"var1-value1", "var1-value2"},
+			"var2": []interface{}{"var2-value1", "var2-value2"},
+			"var3": []interface{}{"var3-value1", "var3-value2"},
+		},
+		"tasks": []interface{}{
+			map[string]interface{}{
+				"task": "task1",
+				"key1": "{{ .v1 }}",
+				"key2": "{{ .v2 }}",
+				"foreach": []interface{}{
+					map[string]interface{}{
+						"variable": "var1",
+						"as":       "v1",
+					},
+					map[string]interface{}{
+						"variable": "var2",
+						"as":       "v2",
+						"foreach": []interface{}{
+							map[string]interface{}{
+								"variable": "var3",
+								"as":       "v3",
+							},
+						},
+					},
+				},
+			},
+		},
+	}
+	expected := []map[string]interface{}{
+		{
+			"task": "task1",
+			"key1": "var1-value1",
+			"key2": "var2-value1",
+		},
+		{
+			"task": "task1",
+			"key1": "var1-value1",
+			"key2": "var2-value2",
+		},
+		{
+			"task": "task1",
+			"key1": "var1-value2",
+			"key2": "var2-value1",
+		},
+		{
+			"task": "task1",
+			"key1": "var1-value2",
+			"key2": "var2-value2",
+		},
+	}
+	actual := workflow.ProcessWorkflow(input)
+	if !reflect.DeepEqual(expected, actual) {
+		t.Errorf("Expected %v, but got %v", expected, actual)
+	}
+}

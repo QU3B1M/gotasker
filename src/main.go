7b23b168900ea1079b08994c1c63eec9d9302512
quebim <kevinledesmam95@gmail.com>
2024-08-31 02:11:13 -0300

    Add GHA for unit tests execution and coverage report

--- src/main.go
+++ src/main.go
@@ -3,7 +3,7 @@ package main
 
 // func main() {
 // 	// TODO: Initialize workflowFilePath, dryRun, and threads
-// 	workflowFilePath := "/home/quebim/gotasker/examples/test.yaml"
+// 	workflowFilePath := "/gotasker/examples/test.yaml"
 // 	dryRun := false
 // 	threads := 0
 // 	workflowProcessor := NewWorkflowProcessor(workflowFilePath, dryRun, threads)
